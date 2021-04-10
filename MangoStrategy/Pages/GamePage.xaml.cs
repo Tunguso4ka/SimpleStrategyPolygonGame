ca3943cffc76c5e56d34d8a14d8678d45fa59077
Tunguso4ka <71643624+Tunguso4ka@users.noreply.github.com>
2021-04-10 12:56:57 +0300

    M 0.0.4 / / i <3 Stef

--- MangoStrategy/Pages/GamePage.xaml.cs
+++ MangoStrategy/Pages/GamePage.xaml.cs
@@ -21,16 +21,17 @@ namespace MangoStrategy
     public partial class GamePage : Page
     {
         List<Ellipse> Ellipses = new List<Ellipse>();
+        List<Path> Paths = new List<Path>();
         MainWindow _this;
         Point _Point;
-        bool PointChanged;
 
         public GamePage(MainWindow Recived_this)
         {
             InitializeComponent();
             _this = Recived_this;
-            AddCity(50, 100, Brushes.Red);
+            AddCity(720, 230, Brushes.Red);
             AddCity(1000, 1000, Brushes.Red);
+            AddPath(634, 202, 619, 216, Brushes.Red);
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -84,6 +85,11 @@ namespace MangoStrategy
                 _Ellipse.Width = 0.2 * _Slider.Value;
                 MapCanvas.Children.Add(_Ellipse);
             }
+            for (int i = 0; i < Paths.Count; i++)
+            {
+                Path _Path = Paths[i];
+                MapCanvas.Children.Add(_Path);
+            }
         }
 
         private void MapViewbox_MouseDown(object sender, MouseButtonEventArgs e)
@@ -94,16 +100,43 @@ namespace MangoStrategy
 
         public async void AddCityByClick(Brush CountryBrush)
         {
-            PointChanged = false;
-            int i = 50;
+            int i = 500;
             while(i != 0)
             {
-                await Task.Delay(100);
+                await Task.Delay(10);
                 _this.ConsoleTextBox.Text = "Click on Map. Time: " + Convert.ToString(i) + " X:" + _Point.X + " Y:" + _Point.Y;
                 i--;
             }
 
             AddCity((int)_Point.X, (int)_Point.Y, CountryBrush);
+        }
+
+        public async void AddPathByClick(Brush CountryBrush)
+        {
+            int i = 500;
+            while (i != 0)
+            {
+                await Task.Delay(10);
+                _this.ConsoleTextBox.Text = "Click on Map. Time: " + Convert.ToString(i) + " X:" + _Point.X + " Y:" + _Point.Y;
+                i--;
+            }
+
+            //AddPath((int)_Point.X, (int)_Point.Y, CountryBrush);
+        }
+
+        public void AddPath(int X0, int Y0, int X1, int Y1, Brush CountryBrush)
+        {
+            Path _Path = new Path();
+            _Path.Stroke = CountryBrush;
+            _Path.StrokeThickness = 2;
+            Geometry _Geometry = Geometry.Parse("M " + Convert.ToString(X0) + "," + Convert.ToString(Y0) + " " + Convert.ToString(X1) + "," + Convert.ToString(Y1));
+            _Path.Data = _Geometry;
+
+            MapCanvas.Children.Add(_Path);
+
+            Paths.Add(_Path);
+
+            _this.ConsoleTextBox.Text = "Map AddPath " + Convert.ToString(X1) + " " + Convert.ToString(Y1);
         }
     }
 }
